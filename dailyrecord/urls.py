970e76e0fdba9828c10122b9ab2daab6fd120dad
Yebin11 <55072785+Yebin11@users.noreply.github.com>
2021-07-09 21:10:39 +0900

    Update urls.py

--- dailyrecord/urls.py
+++ dailyrecord/urls.py
@@ -6,5 +6,7 @@ app_name = 'daily_record'
 
 urlpatterns = [
     path('', views.index),
+    path('login/', views.loginP),
+    path('join/', views.joinP),
     path('header/', views.header, name='header')
 ]
